6b2fb25300e2354cc851bb0c8f4f065cf7f85c35
WinterSleep1 <131164420+WinterSleep1@users.noreply.github.com>
2025-02-13 19:39:23 +0900

    Add files via upload

---
+++ Program3/Program3/Program3.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class SingleLinkedList
+{
+private:
+	int size;
+
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+
+	Node* head;
+
+public:
+	SingleLinkedList()
+	{
+		size = 0;
+		head = nullptr;
+	}
+
+	void push_front(T data)		//	가장 앞에 생성
+	{
+		Node* newNode = new Node;
+
+		newNode->data = data;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->next = nullptr;
+		}
+		else
+		{
+			newNode->next = head;
+			head = newNode;	
+			size++;
+		}
+	}
+
+	void push_back(T data)		//	가장 뒤에 생성
+	{
+		Node* newNode = new Node;
+		Node* currentNode;
+
+		newNode->data = data;
+
+		if (head == nullptr)			//	첫 생성
+		{
+			head = newNode;
+
+			newNode->next = nullptr;
+		}
+		else
+		{
+
+		}
+	}
+
+	// ~SingleLinkedList() {}
+};
+
+int main()
+{
+	SingleLinkedList<int> singleLinkedList;
+
+	singleLinkedList.push_front(10);
+	singleLinkedList.push_front(20);
+
+	cout <<  << endl;
+
+	return 0;
+}
