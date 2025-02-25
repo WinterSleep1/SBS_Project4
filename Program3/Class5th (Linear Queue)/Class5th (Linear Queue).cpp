5ed15c4b004e5bbb029e7d15a738cb08c61a08a4
WinterSleep1 <131164420+WinterSleep1@users.noreply.github.com>
2025-02-25 19:43:15 +0900

    Add files via upload

---
+++ Program3/Class5th (Linear Queue)/Class5th (Linear Queue).cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+template<typename T>
+class LinearQueue
+{
+private:
+	int rare;
+	int front;
+
+	T ary[SIZE];
+
+public:
+	LinearQueue()
+	{
+		rare = 0;
+		front = 0;
+
+		for (int i = 0; i < size; i++) { ary[i] = 0; }
+	}
+
+	void push(T data)
+	{
+		if (rare == SIZE) cout << "Over Flow" << endl;
+		else			  ary[rare++] = data;
+	}
+	void pop()
+	{
+		if (empty()) cout << "Empty" << endl;
+		else ary[front++] = NULL;
+	}
+
+	const T& front()
+	{
+		if (empty())
+		{
+			exit(1);
+		}
+		else
+		{
+			return ary[front];
+		}
+	}
+	const int& Size()
+	{
+		return rare - front;
+	}
+
+	bool empty()
+	{
+		if (front == rare) return true;
+		else return false;
+	}
+};
+
+int main()
+{
+	LinearQueue<int> linearqueue;
+
+	linearqueue.push(10);
+
+	return 0;
+}
