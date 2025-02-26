935fbb740257fcde7170b3bc8654f367c2df9287
WinterSleep1 <131164420+WinterSleep1@users.noreply.github.com>
2025-02-26 19:44:13 +0900

    Add files via upload

---
+++ Program3/Class6th (Circle Queue)/Class6th (Circle Queue).cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+#define SIZE 4
+
+using namespace std;
+
+template<typename T>
+class CircleQueue
+{
+private:
+	int front;
+	int rear;
+	T ary[SIZE];
+
+public:
+	CircleQueue()
+	{
+		rear = SIZE - 1;
+		front = SIZE - 1;
+		for (int i = 0; i < SIZE; i++) { ary[i] = NULL; }
+	}
+
+	void Push(T data)
+	{
+
+	}
+
+	void Pop()
+	{
+
+	}
+	bool Empty()
+	{
+
+	}
+	const T& Front()
+	{
+
+	}
+	const int& Size()
+	{
+
+	}
+};
+
+int main()
+{
+	CircleQueue<int> circlequeue;
+
+
+
+	return 0;
+}
